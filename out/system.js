22d271b25db5355988a75244cc1892f7d9fc82c4
freewebtime <freewebtime@gmail.com>
2019-04-19 23:57:42 +0300

    migrated to storytailor 0.0.7

--- out/system.js
+++ out/system.js
@@ -1,19 +1,17 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let __env = require(`./environment`);
+let __env = require(`\.\.\\src/environment`);
 let __context = { __text: [] };
 let __serializer = __env.getSerializer();
+
 __context['ссылка'] = function (урл, текст) {
-    return `[${__serializer.serialize(текст, '\r\n')}](${__serializer.serialize(урл, '\r\n')})`;
+return `[${__serializer.serialize( текст, '\r\n' )}](${__serializer.serialize( урл, '\r\n' )})`;
 };
 __context['фото'] = function (урл, текст, imgRoot) {
-    if (!imgRoot) {
-        imgRoot = `https:\//raw.githubusercontent.com/freewebtime/storyscriptOrigins/master/resources/`;
-    }
-    ;
-    return `![${__serializer.serialize(текст, '\r\n')}](${__serializer.serialize(imgRoot, '\r\n')}${__serializer.serialize(урл, '\r\n')})`;
+if (!imgRoot) { 
+	imgRoot = `https:\//raw.githubusercontent.com/freewebtime/storyscriptOrigins/master/resources/`;
+ };
+return `![${__serializer.serialize( текст, '\r\n' )}](${__serializer.serialize( imgRoot, '\r\n' )}${__serializer.serialize( урл, '\r\n' )})`;
 };
+
+
 // INFO: this trick is for making this file node module
-exports.default = __context;
-module.exports = Object.assign({}, __context);
-//# sourceMappingURL=system.js.map
+module.exports = __context;
